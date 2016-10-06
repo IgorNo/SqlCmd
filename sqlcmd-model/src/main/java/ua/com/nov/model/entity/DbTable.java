fc9e1aa9c961653e31283de8ffc5786bdd4492af
IgorNo <novikof1@inbox.ru>
2016-10-06 23:29:37 +0300

    Refactoring entity classes

--- sqlcmd-model/src/main/java/ua/com/nov/model/entity/DbTable.java
+++ sqlcmd-model/src/main/java/ua/com/nov/model/entity/DbTable.java
@@ -1,27 +1,29 @@
 package ua.com.nov.model.entity;
 
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class DbTable {
     private DataBase db;
+    private String tableName;
     private String schemaName;
-    private Column[] columns;
+    private List<Column> columns;
+    private int indexPrimaryKey; // index of primary key in 'columns'
 
     private static class Column {
         private final String colName;
         private final String colTypeName;
-        private final int colScale;
         private final int colPresicion;
+        private final int colScale;
+        private boolean isAutoIncrement;
+        private int isNullable; // 1 - column No Nulls
 
-
-        public Column(String colName, String colTypeName, int colScale, int colPresicion) {
+        public Column(String colName, String colTypeName, int colPresicion, int colScale, boolean isAutoIncrement, int isNullable) {
             this.colName = colName;
             this.colTypeName = colTypeName;
-            this.colScale = colScale;
             this.colPresicion = colPresicion;
+            this.colScale = colScale;
+            this.isAutoIncrement = isAutoIncrement;
+            this.isNullable = isNullable;
         }
 
         public String getColName() {
@@ -41,29 +43,41 @@ public class DbTable {
         }
     }
 
-    public DbTable(DataBase db, ResultSetMetaData metaData) throws SQLException {
+    public DbTable(DataBase db, String schemaName, String tableName) {
         this.db = db;
-        this.schemaName = metaData.getSchemaName(1);
-        columns = new Column[metaData.getColumnCount()];
-        for (int i = 0; i < columns.length; i++) {
-            columns[i] = new Column(
-                    metaData.getColumnName(i+1),
-                    metaData.getColumnTypeName(i+1),
-                    metaData.getScale(i+1),
-                    metaData.getPrecision(i+1)
-            );
-        }
+        this.schemaName = schemaName;
+        this.tableName = tableName;
     }
 
     public DataBase getDb() {
         return db;
     }
 
+    public String getTableName() {
+        return tableName;
+    }
+
+    public String getSchemaName() {
+        return schemaName;
+    }
+
+    public boolean addColumn(Column column) {
+        return columns.add(column);
+    }
+
     public int getNumberColumns() {
-        return columns.length;
+        return columns.size();
     }
     
     public String getColumnName(int index) {
-        return columns[index].getColName();
+        return columns.get(index).getColName();
+    }
+
+    public int getColumnIndex(String columnName) {
+        for (int i = 0; i < columns.size(); i++) {
+            if (columns.get(i).colName.equals(columnName))
+                return i;
+        }
+        throw new IllegalArgumentException(String.format("Column %s doesn't exist in table %s", columnName, tableName));
     }
 }
--- sqlcmd-model/src/main/java/ua/com/nov/model/entity/RowTable.java
+++ sqlcmd-model/src/main/java/ua/com/nov/model/entity/RowTable.java
@@ -1,22 +1,16 @@
 package ua.com.nov.model.entity;
 
-import java.util.Map;
-
 public class RowTable {
     private DbTable table;
     private Object[] values;
 
     public RowTable(DbTable table, Object[] values) {
         this.table = table;
-        this.values = values;
+        this.values = new Object[table.getNumberColumns()];
     }
 
-    public String getColumnName(int index) {
-        return table.getColumnName(index);
-    }
-
-    public Object getColumnValue(int index) {
-        return values[index];
+    public Object getColumnValue(String columnName) {
+        return values[table.getColumnIndex(columnName)];
     }
 
 }
