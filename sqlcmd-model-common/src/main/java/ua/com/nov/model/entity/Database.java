037fe5117c2e60482f9fef1c30c705b0320cec02
IgorNo <novikof1@inbox.ru>
2016-11-16 10:52:33 +0200

    Refactoring Database classes  - add DatabasePK

--- sqlcmd-model-common/src/main/java/ua/com/nov/model/entity/Database.java
+++ sqlcmd-model-common/src/main/java/ua/com/nov/model/entity/Database.java
@@ -3,32 +3,29 @@ package ua.com.nov.model.entity;
 import ua.com.nov.model.util.DataSourceUtil;
 
 public class Database {
-    private final String dbUrl;
-    private String userName;
+    private DatabasePK pk;
     private String password;
     private String properties = "";
 
-    public Database(String dbUrl) {
-        this(dbUrl, null, null);
+    public Database(DatabasePK pk) {
+        this(pk, null);
     }
 
-    public Database(String dbUrl, String userName, String password) {
-        if (dbUrl == null || "".equals(dbUrl)) throw new IllegalArgumentException();
-        this.dbUrl = dbUrl;
-        this.userName = userName;
+    public Database(DatabasePK pk, String password) {
+        this.pk = pk;
         this.password = password;
     }
 
     public String getDbUrl() {
-        return dbUrl;
+        return pk.getDbUrl();
     }
 
     public String getName() {
-        return DataSourceUtil.getDatabaseName(dbUrl);
+        return DataSourceUtil.getDatabaseName(getDbUrl());
     }
 
     public String getUserName() {
-        return userName;
+        return pk.getUserName();
     }
 
     public String getPassword() {
@@ -39,30 +36,8 @@ public class Database {
         return properties;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public void setProperties(String properties) {
         this.properties = properties;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Database database = (Database) o;
-
-        return dbUrl.equals(database.dbUrl);
-    }
-
-    @Override
-    public int hashCode() {
-        return dbUrl.hashCode();
-    }
 }
---
+++ sqlcmd-model-common/src/main/java/ua/com/nov/model/entity/DatabasePK.java
@@ -0,0 +1,38 @@
+package ua.com.nov.model.entity;
+
+public class DatabasePK {
+    private String dbUrl;
+    private String userName;
+
+    public DatabasePK(String dbUrl, String userName) {
+        this.dbUrl = dbUrl;
+        this.userName = userName;
+    }
+
+    public String getDbUrl() {
+        return dbUrl;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        DatabasePK that = (DatabasePK) o;
+
+        if (!dbUrl.equals(that.dbUrl)) return false;
+        return userName.equals(that.userName);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = dbUrl.hashCode();
+        result = 31 * result + userName.hashCode();
+        return result;
+    }
+}
