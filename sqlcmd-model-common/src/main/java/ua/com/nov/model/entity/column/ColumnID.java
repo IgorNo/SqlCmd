b323b8972492054be58e02c77e40a1519ba76dcb
IgorNo <novikof1@inbox.ru>
2017-02-23 23:53:53 +0200

    Refactoring Column classes after Great refactoring Dao and SqlStatement source interfaces

--- sqlcmd-model-common/src/main/java/ua/com/nov/model/entity/column/ColumnID.java
+++ sqlcmd-model-common/src/main/java/ua/com/nov/model/entity/column/ColumnID.java
@@ -1,26 +1,22 @@
 package ua.com.nov.model.entity.column;
 
-import ua.com.nov.model.entity.table.TableID;
+import ua.com.nov.model.entity.table.Table;
 
-public class ColumnID {
-    private TableID tableID; // identificator of column's table
-    private String name;     // column name
+public class ColumnId {
+    private Table table;
+    private String name;
 
-    public ColumnID(TableID tableID, String name) {
-        this.tableID = tableID;
+    public ColumnId(Table table, String name) {
+        this.table = table;
         this.name = name;
     }
 
-    public TableID getTableID() {
-        return tableID;
+    public Table getTable() {
+        return table;
     }
 
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
     
 }
