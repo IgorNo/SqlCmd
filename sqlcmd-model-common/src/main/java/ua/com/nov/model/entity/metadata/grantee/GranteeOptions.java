2ffbe56ce5132777db50d20f399c12249a1e579c
IgorNo <novikof1@inbox.ru>
2018-01-03 17:52:33 +0200

    Add 'Table' in table meta data and remove pakage 'metadata' into another pakage

--- sqlcmd-model-common/src/main/java/ua/com/nov/model/entity/metadata/grantee/GranteeOptions.java
+++
@@ -1,9 +0,0 @@
-package ua.com.nov.model.entity.metadata.grantee;
-
-import ua.com.nov.model.entity.MetaDataOptions;
-
-public abstract class GranteeOptions<E extends Grantee> extends MetaDataOptions<E> {
-    public GranteeOptions(Builder builder) {
-        super(builder);
-    }
-}
--- sqlcmd-model-common/src/main/java/ua/com/nov/model/entity/metadata/grantee/privelege/OnDeleteOptions.java
+++
@@ -1,5 +0,0 @@
-package ua.com.nov.model.entity.metadata.grantee.privelege;
-
-public enum OnDeleteOptions {
-    CASCADE, RESTRICT;
-}
